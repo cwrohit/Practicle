b5ea85ce8f04ff2070018afb363ebca4cea6c3e6
Rohit Yadav <122785331+cwrohit@users.noreply.github.com>
2023-01-20 17:42:49 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,22 @@
+public class Operator
+
+{
+public static void main(String[] args)
+{
+
+//int a = 10;
+//int b = 15 + a;
+//System.out.println(a);
+
+int b = 9;
+b *=2;
+System.out.println(b);
+
+//LOGICAL OPERATOR
+
+System.out.println(64<10);
+System.out.println(64>50 && 64>63);
+System.out.println(64>500 || 64>63);
+
+}
+}
