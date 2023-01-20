e9c8105ca4cff287177ed9c3d016b07ef883d7ab
Rohit Yadav <122785331+cwrohit@users.noreply.github.com>
2023-01-20 17:43:37 +0530

    Add files via upload

---
+++ Math.java
@@ -0,0 +1,15 @@
+class Math
+{
+public static void main(String[]abc)
+{
+int a = 2;
+int b = 3;
+System.out.println(a>b&&b<a);
+System.out.println(a<b&&b>a);
+System.out.println(a<b&&b==a);
+
+System.out.println(a>b||b>a);
+System.out.println(a<b||b==a);
+System.out.println(a>b||b<a);
+}
+}
