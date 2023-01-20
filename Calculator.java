016bf50cfcc1f71b250f1dbf6461e1d08b9561b4
Rohit Yadav <122785331+cwrohit@users.noreply.github.com>
2023-01-20 17:08:43 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+class Calculator
+{
+public static void main(String[]abc)
+{
+System.out.println("a+b,a-b,a*b,a/b :");
+int a = 50;
+int b = 25;
+int c = a+b;
+int d = a-b;
+int e = a*b;
+int f = a/b;
+System.out.println("the sum is " + c);
+System.out.println("the sub is " + d);
+System.out.println("the mul is " + e);
+System.out.println("the div is " + f);
+
+}
+}
